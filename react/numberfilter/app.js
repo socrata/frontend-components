730b7475ac4716047b18ed0916449a66f2fb2565
H. Omer Sensoy <omer.sensoy@vngrs.com>
2015-09-16 22:11:33 +0300

    CORE-6729 React component - Numberrange filter

---
+++ react/numberfilter/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import SocrataNumberfilter from './src/react.socrata.numberfilter';
+
+React.render(<SocrataNumberfilter name='Licence Costs'/>,
+  document.getElementById('app')
+);

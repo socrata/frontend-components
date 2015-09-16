78a6378b44d79e280ebff74ffd22e214e184e2a0
H. Omer Sensoy <omer.sensoy@vngrs.com>
2015-09-16 22:11:33 +0300

    CORE-6729 React component - Numberrange filter

---
+++ react/numberfilter/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: './example/',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!autoprefixer-loader!sass'
+      }
+    ]
+  }
+}

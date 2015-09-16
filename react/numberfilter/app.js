10af5ad33aba5ae730f93e29e5c5d2f4a9d6d474
H. Omer Sensoy <omer.sensoy@vngrs.com>
2015-09-16 23:11:40 +0300

    CORE-6729 files are deleted from root

--- react/numberfilter/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import SocrataNumberfilter from './src/react.socrata.numberfilter';
-
-React.render(<SocrataNumberfilter name='Licence Costs'/>,
-  document.getElementById('app')
-);
